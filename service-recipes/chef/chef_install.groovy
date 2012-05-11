0f06bbfa91eec735d5b0e7ea93280a1042383a06
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-05-11 13:29:40 +0300

    Bug fixes

--- service-recipes/chef/chef_install.groovy
+++ service-recipes/chef/chef_install.groovy
@@ -1,1 +1,15 @@
-ChefBootstrap.getBootstrap().runClient(["role[${context.getServiceName()}]"])
+import org.cloudifysource.dsl.context.ServiceContextFactory
+import java.util.concurrent.TimeUnit
+
+def context = ServiceContextFactory.getServiceContext()
+def chef_server_service = context.waitForService("chef-server", 20, TimeUnit.SECONDS)
+def chefServerURL = "http://${chef_server_service.instances[0].hostName}:4000".toString()
+def validationCert = context.attributes.thisApplication["chef_validation.pem"]
+
+println "Using Chef server URL: ${chefServerURL}"
+
+ChefBootstrap.getBootstrap(
+    serverURL: chefServerURL,
+    validationCert: validationCert,
+    context: context
+).runClient([run_list: "role[${context.serviceName}]".toString()])
