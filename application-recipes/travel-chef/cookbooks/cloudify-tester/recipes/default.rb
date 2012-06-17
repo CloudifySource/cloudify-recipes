8e86ceb83c9c73407fb9977b4438b7765818fbdb
Yoni Yalovitsky <yoni@fewbytes.com>
2012-06-17 13:06:53 +0300

    first test commit of the tester cookbook to be clone to the mysql service

---
+++ application-recipes/travel-chef/cookbooks/cloudify-tester/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: cloudify-tester
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+gem_package cucumber
+
+remote_directory "/home/ubuntu/cucumber" do
+  source "cucumber"
+  files_owner "ubuntu"
+  files_group "ubuntu"
+  files_mode "0777"
+  owner "ubuntu"
+  group "ubuntu"
+  mode "0777"
+end
