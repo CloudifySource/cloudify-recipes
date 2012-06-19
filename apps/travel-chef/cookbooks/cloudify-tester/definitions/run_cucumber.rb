f26e7bbfe93e7bce24e871d947e7f1c191f85488
Yoni Yalovitsky <yoni@fewbytes.com>
2012-06-19 12:55:50 +0300

    syntax fix

--- apps/travel-chef/cookbooks/cloudify-tester/definitions/run_cucumber.rb
+++ apps/travel-chef/cookbooks/cloudify-tester/definitions/run_cucumber.rb
@@ -1,8 +1,8 @@
 define :run_cucumber, :feature => nil do
-  bash "run cucumber feature /#{params[:name]}/#{params[:feature]}"
+  execute "run cucumber feature /#{params[:name]}/#{params[:feature]}" do
     user "root"
     cwd node['cloudify-tester']['cucumber_dir']
-    code "jruby -S cucumber features/#{params[:name]}/#{params[:feature]}.feature"
+    command "jruby -S cucumber features/#{params[:name]}/#{params[:feature]}.feature"
     ignore_failure true
-  end
+  end 
 end
