ce0438bc1124434fbc455fd2739a45870c81dcca
Yoni Yalovitsky <yoni@fewbytes.com>
2012-06-21 20:39:19 +0300

    added tomcat jmx tests, still can't get right MBean names

--- apps/travel-chef/cookbooks/cloudify-tester/files/default/tomcat/localhost.feature
+++ apps/travel-chef/cookbooks/cloudify-tester/files/default/tomcat/localhost.feature
@@ -3,6 +3,17 @@ Feature: local tomcat instance
 
   Scenario: check the thread pool
     Given I have JMX exposed locally on port 11099
-        And I have the object 'Catalina:name=http-8080,type=ThreadPool'
-    Then it should have attribute 'maxThreads' with value '200'
-
+    When I examine the object "Catalina:name=http-8080,type=ThreadPool"
+    Then the attribute "maxThreads" is equal to 200
+    And the attribute "currentThreadsBusy" is less than 150
+    
+  Scenario: check the garbage collector
+    Given I have JMX exposed locally on port 11099
+    When I examine the object "java.lang:name=Copy,type=GarbageCollector"
+    Then the attribute "CollectionCount" is between 10 and 50
+    
+  Scenario: check the request processor
+    Given I have JMX exposed locally on port 11099
+    When I examine the object "Catalina:name=http-8080,type=GlobalRequestProcessor"
+    Then the attribute "maxTime" is between 5 and 20
+    And the attribute "errorCount" is equal to 0
--- apps/travel-chef/cookbooks/cloudify-tester/files/default/tomcat/steps/jmx_steps.rb
+++ apps/travel-chef/cookbooks/cloudify-tester/files/default/tomcat/steps/jmx_steps.rb
@@ -1,22 +1,37 @@
 require 'rubygems'
 require 'jmx4r'
 
+def massage_attribute(att)
+  #jmx4r uses underscore_separation instead of Java's camelCase
+  att.gsub(/[A-Z]/, '_\0').downcase
+end
+
 Given /^I have JMX exposed locally on port (\d+)$/ do |port|
   JMX::MBean.establish_connection :host => "localhost", :port => port
 end
 
-Given /^I have JMX exposed on port (\d+) of '(.*?)' $/ do |port, host|
+Given /^I have JMX exposed on port (\d+) of (.*?) $/ do |port, host|
   JMX::MBean.establish_connection :host => host, :port => port
 end
 
-Given /^I have the object '(.*?)'$/ do |jmx_name|
+When /^I examine the object "(.*?)"$/ do |jmx_name|
   @jmx_obj = JMX::MBean.find_by_name jmx_name
 end
 
-Then /^it should have attribute '(.*?)' with value '(.*?)'$/ do |attribute, expected_value|
-  #jmx4r uses underscore_separation instead of Java's camelCase
-  massaged_attribute = attribute.gsub(/[A-Z]/, '_\0').downcase
-  
-  @jmx_obj.send(massaged_attribute).to_s.should == expected_value
+Then /^the attribute "(.*?)" is equal to (.*?)$/ do |attribute, expected_value|
+  @jmx_obj.send(massage_attribute(attribute)).to_s.should == expected_value
 end
 
+Then /^the attribute "(.*?)" is at least (.*?)$/ do |attribute, expected_value|
+  @jmx_obj.send(massage_attribute(attribute)).to_f.should >= expected_value.to_f
+end
+
+Then /^the attribute "(.*?)" is less than (.*?)$/ do |attribute, expected_value|
+  @jmx_obj.send(massage_attribute(attribute)).to_f.should < expected_value.to_f
+end
+
+Then /^the attribute "(.*?)" is between (.*?) and (.*?)$/ do |attribute, min_value, max_value|
+  value = @jmx_obj.send(massage_attribute(attribute)).to_f
+  value.should <= expected_value.to_f
+  value.should >= expected_value.to_f
+end
