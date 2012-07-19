cc959e1a00095432ca9ceaba3020b615566e855e
Yoram Weinreb <yoram@gigaspaces.com>
2012-07-19 11:12:24 +0300

    ginsights deleted

--- apps/biginsights/hbase/hbase_start.groovy
+++
@@ -1,8 +0,0 @@
-import org.cloudifysource.dsl.context.ServiceContextFactory
-import java.util.concurrent.TimeUnit
-
-println "hbase_start.groovy: BigInsights hbase node is about to start"
-
-sleep 7200000
-
-println "hbase_start.groovy: BigInsights hbase node is about to end"
