2ffbb5ed5b3ad2eb0518387e79e18868162b842c
nirbir <nirb@nirb-pc.gspaces.com>
2012-10-15 19:30:22 +0200

    fixed and refactored the example

--- apps/streaming-bigdata/bigDataApp/processor/processor-service.groovy
+++ apps/streaming-bigdata/bigDataApp/processor/processor-service.groovy
@@ -1,12 +1,12 @@
 service {
   name "processor"
   numInstances 2
-  maxAllowedInstances 2
+  maxAllowedInstances 4
   statefulProcessingUnit {
     binaries "rt-analytics-processor.jar"   		
     sla {
       memoryCapacity 32
-      maxMemoryCapacity 32
+      maxMemoryCapacity 64
       highlyAvailable true
       memoryCapacityPerContainer 16 
     }
