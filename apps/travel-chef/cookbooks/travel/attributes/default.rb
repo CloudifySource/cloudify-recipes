a969c68d0ff487177dbf110dc2cadcc8b17accb1
Uri Cohen <uri1803@gmail.com>
2012-10-31 16:03:51 +0200

    Change the war file url to point to gigaspaces CDN URL and not directly to S3

--- apps/travel-chef/cookbooks/travel/attributes/default.rb
+++ apps/travel-chef/cookbooks/travel/attributes/default.rb
@@ -4,5 +4,5 @@ default["travel"]["db_pass"] = "test"
 
 default["travel"]["war_directory"] = "/opt/travel"
 default["travel"]["war_name"] = "travel.war"
-default["travel"]["war_url"] = "http://s3.amazonaws.com/gigaspaces-repository/org/cloudifysource/sample-apps/travel.war"
+default["travel"]["war_url"] = "http://repository.cloudifysource.org/org/cloudifysource/sample-apps/travel.war"
 default["travel"]["war_checksum"] = "a7325aa316663cd2b9c4bf8d964114b50da889216f83c6be9fcc2405ca837096"
