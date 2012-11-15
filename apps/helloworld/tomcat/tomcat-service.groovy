b0a9db283ca205f18695bf60a4bfa21e1c06b755
Guy Korland <gkorland@gmail.com>
2012-11-15 15:30:43 +0200

    format file

--- apps/helloworld/tomcat/tomcat-service.groovy
+++ apps/helloworld/tomcat/tomcat-service.groovy
@@ -16,50 +16,50 @@
 import static JmxMonitors.*
 
 service {
-	extend "tomcat"		
+	extend "tomcat"
 	elastic false
 	numInstances 1
 	minAllowedInstances 1
 	maxAllowedInstances 1
-	
-	def portIncrement =  context.isLocalCloud() ? context.getInstanceId()-1 : 0		
-	
+
+	def portIncrement =  context.isLocalCloud() ? context.getInstanceId()-1 : 0
+
 	def currJmxPort = jmxPort + portIncrement
 	def currHttpPort = port + portIncrement
 	def currAjpPort = ajpPort + portIncrement
-			
+
 	lifecycle {
-	
+
 		details {
 			def currPublicIP
-			
+
 			if (  context.isLocalCloud()  ) {
 				currPublicIP =InetAddress.localHost.hostAddress
 			}
 			else {
 				currPublicIP = System.getenv()["CLOUDIFY_AGENT_ENV_PUBLIC_IP"]
 			}
-			def tomcatURL = "http://${currPublicIP}:${currHttpPort}"	
-						
+			def tomcatURL = "http://${currPublicIP}:${currHttpPort}"
+
 			def applicationURL = "${tomcatURL}/${ctxPath}"
 			println "tomcat-service.groovy: applicationURL is ${applicationURL}"
-		
+
 			return [
 				"Application URL":"<a href=\"${applicationURL}\" target=\"_blank\">${applicationURL}</a>"
 			]
-		}	
+		}
 
 		monitors {
-										
+
 			def metricNamesToMBeansNames = [
-				"Current Http Threads Busy": ["Catalina:type=ThreadPool,name=\"http-bio-${currHttpPort}\"", "currentThreadsBusy"],				
-				"Current Http Thread Count": ["Catalina:type=ThreadPool,name=\"http-bio-${currHttpPort}\"", "currentThreadCount"],				
-				"Backlog": ["Catalina:type=ProtocolHandler,port=${currHttpPort}", "backlog"],				
-				"Total Requests Count": ["Catalina:type=GlobalRequestProcessor,name=\"http-bio-${currHttpPort}\"", "requestCount"],				
-				"Active Sessions": ["Catalina:type=Manager,context=/${ctxPath},host=localhost", "activeSessions"],
-			]
-			
-			return getJmxMetrics("127.0.0.1",currJmxPort,metricNamesToMBeansNames)										
-		}								
-	}	
+						"Current Http Threads Busy": ["Catalina:type=ThreadPool,name=\"http-bio-${currHttpPort}\"", "currentThreadsBusy"],
+						"Current Http Thread Count": ["Catalina:type=ThreadPool,name=\"http-bio-${currHttpPort}\"", "currentThreadCount"],
+						"Backlog": ["Catalina:type=ProtocolHandler,port=${currHttpPort}", "backlog"],
+						"Total Requests Count": ["Catalina:type=GlobalRequestProcessor,name=\"http-bio-${currHttpPort}\"", "requestCount"],
+						"Active Sessions": ["Catalina:type=Manager,context=/${ctxPath},host=localhost", "activeSessions"],
+					]
+
+			return getJmxMetrics("127.0.0.1",currJmxPort,metricNamesToMBeansNames)
+		}
+	}
 }
