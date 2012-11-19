637b9ed4501afde3654c25bb91759ec8169ed4c0
Guy Korland <gkorland@gmail.com>
2012-11-19 13:26:59 +0200

    removed full path

--- apps/lamp/apacheLB/apacheLB-service.groovy
+++ apps/lamp/apacheLB/apacheLB-service.groovy
@@ -14,7 +14,7 @@
 * limitations under the License.
 *******************************************************************************/
 service {
-	extend "../../../services/apacheLB"
+	extend "apacheLB"
 	lifecycle {
 	
 	
