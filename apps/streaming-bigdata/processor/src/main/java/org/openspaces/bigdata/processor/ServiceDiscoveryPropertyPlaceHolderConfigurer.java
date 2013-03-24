37e7843ea80ee9059842b2060add384bf21306cd
iliapolo <elip@gigaspaces.com>
2013-03-24 17:29:23 +0200

    CLOUDIFY-1614 fix processor to not use dsl jar, and removed dsl.jar from dep

--- apps/streaming-bigdata/processor/src/main/java/org/openspaces/bigdata/processor/ServiceDiscoveryPropertyPlaceHolderConfigurer.java
+++ apps/streaming-bigdata/processor/src/main/java/org/openspaces/bigdata/processor/ServiceDiscoveryPropertyPlaceHolderConfigurer.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.cloudifysource.dsl.utils.ServiceUtils;
-import org.cloudifysource.dsl.utils.ServiceUtils.FullServiceName;
 import org.openspaces.admin.Admin;
 import org.openspaces.admin.AdminFactory;
 import org.openspaces.admin.pu.ProcessingUnit;
@@ -19,111 +17,119 @@ import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.util.StringUtils;
 
 /**
-  This class is a generic bean that discovers cloudify service instances running on the same application as this processing unit
-  And injects it into a property so it can be used in other bean configurations.
-  
-  For example if service="cassandra" and outputProperty="cassandra.ip-addresses", then in any other bean the string "${cassandra.ip-addresses}"
-  is replaced with a comma seperated list of cassandra instance ip addreses.
+ This class is a generic bean that discovers cloudify service instances running on the same application as this processing unit
+ And injects it into a property so it can be used in other bean configurations.
+
+ For example if service="cassandra" and outputProperty="cassandra.ip-addresses", then in any other bean the string "${cassandra.ip-addresses}"
+ is replaced with a comma seperated list of cassandra instance ip addreses.
  */
 public class ServiceDiscoveryPropertyPlaceHolderConfigurer extends PropertyPlaceholderConfigurer implements ClusterInfoAware , InitializingBean {
-	
-	private String service;
-	private String outputProperty;
-	private long timeoutSeconds = 60;
-	private String[] ipAddresses;
-	private ClusterInfo clusterInfo;
-	private int minimumNumberOfInstances=1;
-	
-	public ServiceDiscoveryPropertyPlaceHolderConfigurer() {
-		super.setIgnoreUnresolvablePlaceholders(true);
-		super.setOrder(0); // medium precedence
-	}
-	
-	@Required
-	public void setService(String service) {
-		this.service = service;
-	}
-		
-	@Required
-	public void setOutputProperty(String property) {
-		this.outputProperty = property;
-	}
-	
-	@Required
-	public void setTimeoutSeconds(long timeoutSeconds) {
-		this.timeoutSeconds = timeoutSeconds;
-	}
-	
-	@Override
-	public void setClusterInfo(ClusterInfo clusterInfo) {
-		this.clusterInfo = clusterInfo;	
-	}
-	
-	@Required
-	public void setMinimumNumberOfInstances(int minimumNumberOfInstances) {
-		this.minimumNumberOfInstances = minimumNumberOfInstances;
-	}
-	
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		final FullServiceName fullName = ServiceUtils
-				.getFullServiceName(clusterInfo.getName());
-		final String application = fullName.getApplicationName();
-		
-		final ProcessingUnitInstance[] instances = waitForInstances(new FullServiceName(application, service), minimumNumberOfInstances, timeoutSeconds, TimeUnit.SECONDS);
-		this.ipAddresses = extractIpAddresses(instances);
-		if (logger.isDebugEnabled()) {
-			logger.debug("Initialized " + getClass() + " with " + outputProperty+"="+ipAddresses);
-		}
-	}
 
-	private static ProcessingUnitInstance[] waitForInstances(final FullServiceName fullServiceName, final int minimumNumberOfInstances, final long timeout, final TimeUnit timeunit) {
-		final ProcessingUnit pu = waitForProcessingUnit(fullServiceName, timeout, timeunit);
-		pu.waitFor(minimumNumberOfInstances, timeout, timeunit);
-		final ProcessingUnitInstance[] instances = pu.getInstances();
-		if (instances.length < minimumNumberOfInstances) {
-			throw new IllegalStateException("Could not discover " + minimumNumberOfInstances + " "+ fullServiceName.getServiceName() + " instances in application " + fullServiceName.getApplicationName());
-		}
-		return instances;
-	}
+    private String service;
+    private String outputProperty;
+    private long timeoutSeconds = 60;
+    private String[] ipAddresses;
+    private ClusterInfo clusterInfo;
+    private int minimumNumberOfInstances=1;
 
-	private static ProcessingUnit waitForProcessingUnit(
-			final FullServiceName fullServiceName, final long timeout,
-			final TimeUnit timeunit) {
-		final Admin admin = new AdminFactory().createAdmin();
-		final String puName = ServiceUtils.getAbsolutePUName(fullServiceName.getApplicationName(), fullServiceName.getServiceName());
-		final ProcessingUnit pu = admin.getProcessingUnits().waitFor(puName, timeout, timeunit);
-		if (pu == null) {
-			throw new IllegalStateException("Could not discover service " + fullServiceName.getServiceName() + " in application " + fullServiceName.getApplicationName());
-		}
-		return pu;
-	}
+    public ServiceDiscoveryPropertyPlaceHolderConfigurer() {
+        super.setIgnoreUnresolvablePlaceholders(true);
+        super.setOrder(0); // medium precedence
+    }
 
-	private static String[] extractIpAddresses(ProcessingUnitInstance[] instances) {
-		final List<String> ipAddresses = new ArrayList<String>(instances.length);
-		for (final ProcessingUnitInstance instance : instances) {
-			ipAddresses.add(instance.getMachine().getHostAddress());
-		}
-		return ipAddresses.toArray(new String[ipAddresses.size()]);
-	}
+    @Required
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    @Required
+    public void setOutputProperty(String property) {
+        this.outputProperty = property;
+    }
+
+    @Required
+    public void setTimeoutSeconds(long timeoutSeconds) {
+        this.timeoutSeconds = timeoutSeconds;
+    }
+
+    @Override
+    public void setClusterInfo(ClusterInfo clusterInfo) {
+        this.clusterInfo = clusterInfo;
+    }
+
+    @Required
+    public void setMinimumNumberOfInstances(int minimumNumberOfInstances) {
+        this.minimumNumberOfInstances = minimumNumberOfInstances;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        final String application = getApplicationName();
+
+        final ProcessingUnitInstance[] instances = waitForInstances(application, service, minimumNumberOfInstances, timeoutSeconds, TimeUnit.SECONDS);
+        this.ipAddresses = extractIpAddresses(instances);
+        if (logger.isDebugEnabled()) {
+            logger.debug("Initialized " + getClass() + " with " + outputProperty+"="+ipAddresses);
+        }
+    }
+
+    // Duplication from cloudify ServiceUtils.
+    // to not include a dependency on the dsl jar.
+    private String getApplicationName() {
+        final int index = clusterInfo.getName().lastIndexOf('.');
+        if (index < 0) {
+            throw new IllegalArgumentException("Could not parse PU name: " + clusterInfo.getName()
+                    + " to read service and application names.");
+        }
+
+        return clusterInfo.getName().substring(0, index);
+    }
+
+    private static ProcessingUnitInstance[] waitForInstances(final String applicationName, final String serviceName, final int minimumNumberOfInstances, final long timeout, final TimeUnit timeunit) {
+        final ProcessingUnit pu = waitForProcessingUnit(applicationName, serviceName, timeout, timeunit);
+        pu.waitFor(minimumNumberOfInstances, timeout, timeunit);
+        final ProcessingUnitInstance[] instances = pu.getInstances();
+        if (instances.length < minimumNumberOfInstances) {
+            throw new IllegalStateException("Could not discover " + minimumNumberOfInstances + " "+ serviceName + " instances in application " + applicationName);
+        }
+        return instances;
+    }
+
+    private static ProcessingUnit waitForProcessingUnit(final String applicationName, final String serviceName,  final long timeout, final TimeUnit timeunit) {
+        final Admin admin = new AdminFactory().createAdmin();
+        final String puName = applicationName + "." + serviceName;
+        final ProcessingUnit pu = admin.getProcessingUnits().waitFor(puName, timeout, timeunit);
+        if (pu == null) {
+            throw new IllegalStateException("Could not discover service " + serviceName + " in application " + applicationName);
+        }
+        return pu;
+    }
+
+    private static String[] extractIpAddresses(ProcessingUnitInstance[] instances) {
+        final List<String> ipAddresses = new ArrayList<String>(instances.length);
+        for (final ProcessingUnitInstance instance : instances) {
+            ipAddresses.add(instance.getMachine().getHostAddress());
+        }
+        return ipAddresses.toArray(new String[ipAddresses.size()]);
+    }
 
 
-	/**
-	 * This implementation resolves hostProperty into hostAddress
-	 */
-	@Override
-	protected String resolvePlaceholder(String placeholder, Properties props) {
-		String value = null;
-		if (placeholder.equals(outputProperty)) {
-			if (ipAddresses == null) {
-				throw new IllegalStateException("Bean not initialized yet");
-			}
-			value = StringUtils.arrayToCommaDelimitedString(ipAddresses);
-			if (logger.isDebugEnabled()) {
-				logger.debug("Resolved " + placeholder +"="+value);
-			}
-		}
-		return value;
-	}
+    /**
+     * This implementation resolves hostProperty into hostAddress
+     */
+    @Override
+    protected String resolvePlaceholder(String placeholder, Properties props) {
+        String value = null;
+        if (placeholder.equals(outputProperty)) {
+            if (ipAddresses == null) {
+                throw new IllegalStateException("Bean not initialized yet");
+            }
+            value = StringUtils.arrayToCommaDelimitedString(ipAddresses);
+            if (logger.isDebugEnabled()) {
+                logger.debug("Resolved " + placeholder +"="+value);
+            }
+        }
+        return value;
+    }
 
 }
