437ba62763883d9c737f6bf270a289de8cb21848
Yoni Yalovitsky <yoni@fewbytes.com>
2013-06-17 20:21:18 +0300

    berkshelf: added gecode cookbook and its dependencies as submodules

--- services/chef/berks-cookbooks/berkshelf/recipes/default.rb
+++ services/chef/berks-cookbooks/berkshelf/recipes/default.rb
@@ -10,5 +10,6 @@ node.set[:languages][:ruby][:default_version] = "1.9.1"
 node.set['build_essential']['compiletime'] = true
 include_recipe "build-essential"
 include_recipe "xml::ruby"
+include_recipe "gecode"
 chef_gem "berkshelf"
 package "git"
