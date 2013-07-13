e6a0b06209ce2856ac0a76e4d6125484789b3b24
Tamir Korem <tamir@gigaspaces.com>
2013-07-13 20:19:53 +0300

    Added import org.cloudifysource.dsl.utils.ServiceUtils

--- services/tomcat/updateWarFile.groovy
+++ services/tomcat/updateWarFile.groovy
@@ -14,6 +14,7 @@
 * limitations under the License.
 *******************************************************************************/
 import org.cloudifysource.dsl.context.ServiceContextFactory
+import org.cloudifysource.dsl.utils.ServiceUtils
 
 println "updateWarFile.groovy: Starting..."
 
