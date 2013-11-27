71dfc8de1e4936c4ec79c172eabefe5b77a83cd7
Tamir Korem <tamir@gigaspaces.com>
2013-11-27 14:24:08 +0200

    Added ohai

---
+++ services/chef-services/cookbooks/metadata.rb
@@ -0,0 +1,11 @@
+name             'cloudify'
+maintainer       'Fewbytes'
+maintainer_email 'avishai@fewbytes.com'
+license          'Apache v2'
+description      'API library for cloudify interop'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'ohai'
+depends          'chef_handler'
+depends          'apt'
