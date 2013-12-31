63f8a1370c449e82a1007b37bcab2faa8dd033b3
kobi <kobkis@gmail.com>
2013-12-31 16:44:15 +0200

    CLOUDIFY-2287 Added tty.js for running ssh via http - fixed path

--- services/xap9x/xap-management/xap_postStart.groovy
+++ services/xap9x/xap-management/xap_postStart.groovy
@@ -6,15 +6,15 @@ config = new ConfigSlurper().parse(new File(context.serviceName+"-service.proper
 
 chmod(dir:"${context.serviceDirectory}/tty.js",perm:"+x",includes:"*.sh")
 new AntBuilder().sequential {
-    exec(executable:"${context.serviceDirectory}/tty.js/init.sh", osfamily:"unix",
+    exec(executable:"${context.serviceDirectory}/init.sh", osfamily:"unix",
             output:"init.${System.currentTimeMillis()}.out",
             error:"init.${System.currentTimeMillis()}.err"
     )
-    exec(executable:"${context.serviceDirectory}/tty.js/install.sh", osfamily:"unix",
+    exec(executable:"${context.serviceDirectory}/install.sh", osfamily:"unix",
             output:"install.${System.currentTimeMillis()}.out",
             error:"install.${System.currentTimeMillis()}.err"
     )
-    exec(executable:"${context.serviceDirectory}/tty.js/start.sh", osfamily:"unix",
+    exec(executable:"${context.serviceDirectory}/start.sh", osfamily:"unix",
             output:"start.${System.currentTimeMillis()}.out",
             error:"start.${System.currentTimeMillis()}.err"
     )
