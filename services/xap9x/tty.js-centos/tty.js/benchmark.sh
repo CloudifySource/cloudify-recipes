93c8a502862595ada67a0a4ef1309d6d1cc81fc3
kobi <kobkis@gmail.com>
2014-01-01 08:54:00 +0200

    CLOUDIFY-2287 run benchmark at tty.js start

--- services/xap9x/tty.js-centos/tty.js/benchmark.sh
+++ services/xap9x/tty.js-centos/tty.js/benchmark.sh
@@ -5,6 +5,11 @@ GRID_NAME=$2
 POJOS_NUMBER=$3
 LOCATORS=$4
 
+echo "$HOSTNAME"
+echo -n "login: "
+read USER
+exec su -l -c '${BENCHMARK_BIN}/run.sh -url jini://*/*/${GRID_NAME}?locators=${LOCATORS} -i ${POJOS_NUMBER}' $USER
+
 ${BENCHMARK_BIN}/run.sh -url jini://*/*/${GRID_NAME}?locators=${LOCATORS} -i ${POJOS_NUMBER}
 
 
