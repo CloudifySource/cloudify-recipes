6584b927591ddfbae1564b75e854002b3ee4f7c4
kobi <kobkis@gmail.com>
2014-01-02 09:47:37 +0200

    CLOUDIFY-2287 Added shell url

--- services/xap9x/tty.js-centos/tty.js/tty.js-service.groovy
+++ services/xap9x/tty.js-centos/tty.js/tty.js-service.groovy
@@ -8,9 +8,6 @@ service {
 
             def shellURL = "http://${currPublicIP}:8080"
             return [
-                    "Application URL":"<a href=\"${applicationURL}\" target=\"_blank\">${applicationURL}</a>"
-            ]
-            return [
                     "Shell":"<a href=\"${shellURL}\" target=\"_blank\">${shellURL}</a>"
             ]
         }
