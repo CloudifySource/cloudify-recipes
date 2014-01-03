7cb20ecdfe730804bd57329417b08057d88daa50
kobi <kobkis@gmail.com>
2014-01-03 18:40:49 +0200

    CLOUDIFY-2287 added debugs

--- services/xap9x/tty.js-centos/tty.js/tty.js_start.groovy
+++ services/xap9x/tty.js-centos/tty.js/tty.js_start.groovy
@@ -11,7 +11,7 @@ config = new ConfigSlurper().parse(new File("tty.js-service.properties").toURL()
 
 new AntBuilder().sequential {
     replace(file:"${context.serviceDirectory}/my-config.json",token:"<XAP_BIN>",value:"${context.serviceDirectory}/${config.installDir}/${config.xapDir}/bin")
-    replace(file:"${context.serviceDirectory}/gs.sh",token:"<XAP_BIN>",value:"${context.serviceDirectory}/${config.installDir}/${config.xapDir}/bin")
+    //replace(file:"${context.serviceDirectory}/gs.sh",token:"<XAP_BIN>",value:"${context.serviceDirectory}/${config.installDir}/${config.xapDir}/bin")
 }
 
 new AntBuilder().sequential {
