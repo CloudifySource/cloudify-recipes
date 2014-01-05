76522aacc52fd5135102898a29a3cd8ad6b504fb
kobi <kobkis@gmail.com>
2014-01-05 16:25:05 +0200

    CLOUDIFY-2287 Added benchmark and bin urls

--- services/xap9x/tty.js-centos/tty.js/start.sh
+++ services/xap9x/tty.js-centos/tty.js/start.sh
@@ -5,8 +5,8 @@ XAB_BIN=$1
 
 export XAP_BIN
 
-tty.js --config gs-config.json --deamonize  &
-tty.js --config benchmark-config.json --deamonize &
-tty.js --config bin-config.json --deamonize &
+#tty.js --config gs-config.json --deamonize
+tty.js --config benchmark-config.json --deamonize
+tty.js --config bin-config.json --deamonize
 
 
