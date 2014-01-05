c723142f44d82792ee73d0adacd2a6ba38240a6f
kobi <kobkis@gmail.com>
2014-01-05 15:00:04 +0200

    CLOUDIFY-2287 Added benchmark and bin urls

--- services/xap9x/tty.js-centos/tty.js/tty.js-service.groovy
+++ services/xap9x/tty.js-centos/tty.js/tty.js-service.groovy
@@ -9,10 +9,12 @@ service {
         details {
             def currPublicIP = context.getPublicAddress()
 
-            def applicationURL = "http://${currPublicIP}:8080"
+            def applicationURL = "http://${currPublicIP}"
 
             return [
-                    "Shell URL":"<a href=\"${applicationURL}\" target=\"_blank\">${applicationURL}</a>"
+                    "GigaSpaces Interactive Shell URL":"<a href=\"${applicationURL}\" target=\"_blank\">${applicationURL}:8080</a>",
+                    "Benchmark URL":"<a href=\"${applicationURL}\" target=\"_blank\">${applicationURL}:8081</a>",
+                    "GigaSpaces URL":"<a href=\"${applicationURL}\" target=\"_blank\">${applicationURL}:8082</a>"
             ]
         }
     }
