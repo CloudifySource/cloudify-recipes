7e037c3022d0fad600669558b1f25f1e27954545
kobikis <kobikis@gmail.com>
2014-01-08 08:32:06 +0200

    fixed urls

--- services/xap9x/tty.js-centos/tty.js/tty.js-service.groovy
+++ services/xap9x/tty.js-centos/tty.js/tty.js-service.groovy
@@ -10,14 +10,10 @@ service {
             def currPublicIP = context.getPublicAddress()
 
             def interactiveShellURL = "http://${currPublicIP}:8080"
-            def benchmarkShellURL = "http://${currPublicIP}:8081"
-            def shellURL = "http://${currPublicIP}:8082"
 
 
             return [
-                    "GigaSpaces Interactive Shell URL":"<a href=\"${interactiveShellURL}\" target=\"_blank\">${interactiveShellURL}</a>",
-                    "Benchmark URL":"<a href=\"${benchmarkShellURL}\" target=\"_blank\">${benchmarkShellURL}</a>",
-                    "GigaSpaces URL":"<a href=\"${shellURL}\" target=\"_blank\">${shellURL}</a>"
+                    "GigaSpaces Interactive Shell URL":"<a href=\"${interactiveShellURL}\" target=\"_blank\">${interactiveShellURL}</a>"
             ]
         }
     }
