0f68298fb94f2ad38acbfc7c4d9c00448d2a9ced
Uri Cohen <uri1803@gmail.com>
2014-01-23 19:03:57 +0200

    Update default.rb

--- apps/travel-chef/cookbooks/travel/attributes/default.rb
+++ apps/travel-chef/cookbooks/travel/attributes/default.rb
@@ -4,5 +4,5 @@ default["travel"]["db_pass"] = "test"
 
 default["travel"]["war_directory"] = "/opt/travel"
 default["travel"]["war_name"] = "travel.war"
-default["travel"]["war_url"] = "http://repository.cloudifysource.org/org/cloudifysource/sample-apps/travel.war"
+default["travel"]["war_url"] = "https://s3-eu-west-1.amazonaws.com/gigaspaces-repository-eu/org/cloudifysource/sample-apps/travel.war"
 default["travel"]["war_checksum"] = "a7325aa316663cd2b9c4bf8d964114b50da889216f83c6be9fcc2405ca837096"
