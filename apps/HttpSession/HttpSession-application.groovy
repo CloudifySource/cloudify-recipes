6df78ad6c123f500d80927aac4a92ea456025e67
Yoram Weinreb <yoram@gigspaces.com>
2014-02-09 15:44:27 +0200

    updated README with demo script and removed the XAP service that was commented out

--- apps/HttpSession/HttpSession-application.groovy
+++ apps/HttpSession/HttpSession-application.groovy
@@ -1,28 +1,13 @@
 application {
 	name="HttpSession"
 	
-/*	service {
-		name = "mgt"	
-	}
-	service {
-		name = "pu"
-		dependsOn = ["mgt"]
-	}
-
-	service {
-		name = "webui"
-		dependsOn = ["mgt"]
-	}	
-*/
 	service {
 		name = "apacheLB"		
 	}
-
 	
 	service {
 		name = "tomcat"
 		dependsOn = ["apacheLB"]
-		//dependsOn = [/*"pu",*/apacheLB"]
 	}
 
 }
