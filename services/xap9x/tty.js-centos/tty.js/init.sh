158e85ac88d412d64858b9d716ff1347a6906afe
yohanakh <yohana@yohanakhoury.com>
2014-03-20 18:49:06 +0200

    Deleted tty.js

--- services/xap9x/tty.js-centos/tty.js/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source $DIR/build.sh
-source $DIR/npm.sh
--- services/xap9x/tty.js-centos/tty.js/npm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-sudo yum -y install npm --enablerepo=epel
