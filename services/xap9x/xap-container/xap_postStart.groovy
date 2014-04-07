992680157481efa0adeccd8c2cb0adb86f1c02c5
yohana <yohana@yohanakhoury.com>
2014-04-07 23:39:57 +0300

    Fixed & Changed mgmt to mgmtService

--- services/xap9x/xap-container/xap_postStart.groovy
+++ services/xap9x/xap-container/xap_postStart.groovy
@@ -6,10 +6,10 @@ context=ServiceContextFactory.serviceContext
 config = new ConfigSlurper().parse(new File(context.serviceName+"-service.properties").toURL())
 
 println "Waiting (max) 2 minutes for ${config.managementService}"
-mgmt=context.waitForService(config.managementService,2,TimeUnit.MINUTES)
-assert (mgmt!=null),"No management services found"
+mgmtService=context.waitForService(config.managementService,2,TimeUnit.MINUTES)
+assert (mgmtService!=null),"No management services found"
 
-mgmtservices = mgmt.waitForInstances(1, 1, TimeUnit.MINUTES)
+mgmtservices = mgmtService.waitForInstances(1, 1, TimeUnit.MINUTES)
 assert (mgmtservices != null), "Unable to find 1 instance of management services"
 
 println "invoking deploy-grid-basic with myDataGrid"
